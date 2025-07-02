a98778f55034a0e7c3700e8750d292ecfe01e7cd
Evgeniy <32774299+KrosteInvise@users.noreply.github.com>
2025-07-02 17:39:49 +0700

    Дата глайданиматора

--- Assets/Scripts/Game/Characters/GlideAnimator.cs
+++ Assets/Scripts/Game/Characters/GlideAnimator.cs
@@ -3,17 +3,23 @@ using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
-using Random = UnityEngine.Random;
 using Vector3 = UnityEngine.Vector3;
 
 namespace Game.Characters
 {
     public class GlideAnimator
     {
+        GlideAnimatorData data;
+        
+        public void Init(GlideAnimatorData data)
+        {
+            this.data = data;
+        }
+        
         public void Glide(Transform tf, TextMeshPro tmpText, SpriteRenderer icon, Action complete = null)
         {
-            float angle = Random.Range(-45f, 0);
-            Vector3 dir = Quaternion.Euler(0, 0, angle) * Vector3.up;
+            Vector3 dir = Quaternion.Euler(0, 0, data.GetRandomAngle()) * new Vector3(0, data.GlideHeight, 0);
+            
             Sequence seq = DOTween.Sequence();
             seq.Append(tf.DOMove(tf.position + dir, .3f)
                 .SetEase(Ease.OutQuad));
---
+++ Assets/Scripts/Game/Characters/GlideAnimatorData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game.Characters
+{
+    public class GlideAnimatorData : ScriptableObject
+    {
+        [SerializeField]
+        float minAngle, maxAngle;
+
+        [SerializeField]
+        float glideHeight;
+
+        public float MinAngle => minAngle;
+
+        public float MaxAngle => maxAngle;
+
+        public float GlideHeight => glideHeight;
+        
+        public float GetRandomAngle()
+        {
+            var angle = Random.Range(minAngle, maxAngle);
+            return angle;
+        }
+    }
+}
